6f354ec8c827dd8326f654bc5dcfc2120852ec4b
Alfe3n <115562186+Alfe3n@users.noreply.github.com>
2023-05-08 20:03:20 +0530

    summary not showing

--- src/pages/gpt/gptSummary.jsx
+++ src/pages/gpt/gptSummary.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+import { useEffect } from "react";
 
 function Gpt(props) {
+  let data = [];
+  const API_KEY = "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";
+  async function fetchdata() {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+
+      headers: {
+        Authorization: `Bearer ${API_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "user",
+            content: `elaborate chapter wise takeaways from the book ${props.name}enclosed in an  js array`,
+          },
+        ],
+      }),
+    });
+    data = await response.json();
+    console.log(data);
+  }
+  useEffect(() => {
+    fetchdata();
+  }, []);
   return (
     <div>
       <h1>{props.name} from gpt</h1>
+      {/* data?<p>{data.choices[0].message.content}</p>:<p>not yet</p> */}
     </div>
   );
 }
