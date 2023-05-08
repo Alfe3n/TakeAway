eb3eeef42d652886c9ea696316135c9ca5e47b50
Alfe3n <115562186+Alfe3n@users.noreply.github.com>
2023-05-08 12:21:26 +0530

    z

--- src/pages/common/navbar.jsx
+++ src/pages/common/navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function navbar() {
   return (
-    <div className=" flex items-center justify-start text-white bg-[#F1F6F4]">
+    <div className="flex items-center justify-start text-white bg-[#F1F6F4]">
       <p className="text-2xl font-bold p-7 text-[#03314B] md:text-5xl ">
         TakeAway
       </p>
