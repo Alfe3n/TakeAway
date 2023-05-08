06469936927960cb6ff7a038d6bd9fc36b96a087
Alfe3n <115562186+Alfe3n@users.noreply.github.com>
2023-05-08 10:05:53 +0530

    aa

--- src/pages/library/categories.jsx
+++ src/pages/library/categories.jsx
@@ -10,7 +10,7 @@ function categories() {
     <div className="flex items-center justify-center w-full">
       <div className="flex flex-wrap justify-center w-1/2 mt-10 categories-container gap-x-3 gap-y-3">
         <button
-          className=" text-[#03314B] bg-[#F1F6F4] p-1 text-sm  rounded-lg md:text-lg md:p-2"
+          className=" text-[#03314B] bg-[#F1F6F4] p-2 text-sm  rounded-lg md:text-lg md:p-2"
           onClick={() => {
             setCategory(["Entrepreneurship"]);
           }}
@@ -18,7 +18,7 @@ function categories() {
           Entrepreneurship
         </button>
         <button
-          className=" bg-[#F1F6F4] text-[#03314B] p-1 text-sm   rounded-lg md:text-lg md:p-2"
+          className=" bg-[#F1F6F4] text-[#03314B] p-2 text-sm   rounded-lg md:text-lg md:p-2"
           onClick={() => {
             setCategory(["Productivity"]);
           }}
@@ -34,7 +34,7 @@ function categories() {
           Communication Skills
         </button>
         <button
-          className="text-[#03314B] p-1 text-sm bg-[#F1F6F4]  rounded-lg md:text-lg md:p-2"
+          className="text-[#03314B] p-2 text-sm bg-[#F1F6F4]  rounded-lg md:text-lg md:p-2"
           onClick={() => {
             setCategory(["Biography"]);
           }}
@@ -42,7 +42,7 @@ function categories() {
           Biography and Memoir
         </button>
         <button
-          className="text-[#03314B] p-1 text-sm bg-[#F1F6F4]  rounded-lg md:text-lg md:p-2"
+          className="text-[#03314B] p-2 text-sm bg-[#F1F6F4]  rounded-lg md:text-lg md:p-2"
           onClick={() => {
             setCategory(["Money"]);
           }}
@@ -50,7 +50,7 @@ function categories() {
           Money & Investment
         </button>
         <button
-          className="text-[#03314B] p-1 text-sm bg-[#F1F6F4]  rounded-lg md:text-lg md:p-2"
+          className="text-[#03314B] p-2 text-sm bg-[#F1F6F4]  rounded-lg md:text-lg md:p-2"
           onClick={() => {
             setCategory(["Philosophy"]);
           }}
